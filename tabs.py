cf34d5bf3c72f79a4158d110a7ba2eab304758f9
JamesBullen <james.roberts.bullen@gmail.com>
2025-04-03 19:24:27 +0100

    Added to StaffTab

--- tabs.py
+++ tabs.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTabWidget, QVBoxLayout, QLineEdit, QLabel, QPushButton, QCheckBox, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTabWidget, QVBoxLayout, QLineEdit, QLabel, QPushButton, QCheckBox, QMessageBox, QDialog
 from database import extractTable
 
 class Tables(QWidget):
@@ -16,13 +16,23 @@ class Tables(QWidget):
 class StaffTab(QWidget):
     def __init__(self, pool):
         super().__init__()
+        self.pool = pool
         layout = QVBoxLayout()
 
+        self.newStaffButton = QPushButton('Add new Staffer', self)
+        self.newStaffButton.clicked.connect(self.addStaff)
+        layout.addWidget(self.newStaffButton)
+
+        # will use different function later
+        results = extractTable(self.pool, 'tbl_staff')
         # Table
-        self.table = Tables(extractTable(pool, 'tbl_staff')[1])
+        self.table = Tables(results[1])
         layout.addWidget(self.table)
 
         self.setLayout(layout)
+
+    def addStaff(self):
+        self.table.hide()
 
 class OrdersTab(QWidget):
     def __init__(self, text):
