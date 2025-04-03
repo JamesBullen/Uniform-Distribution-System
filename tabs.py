d6542e1de3f2c6ca3d9a71c9a7dd85e43f1a7eff
Denvab <denvabgraveiron@gmail.com>
2025-04-03 15:11:51 +0100

    Bug fix

--- tabs.py
+++ tabs.py
@@ -1,24 +1,26 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTabWidget, QVBoxLayout, QLineEdit, QLabel, QPushButton, QCheckBox, QMessageBox
+from database import extractTable
 
 class Tables(QWidget):
-    def __init__(self, columns, headers):
+    def __init__(self, headers):
         super().__init__()
         layout = QVBoxLayout()
 
         self.table = QTableWidget()
-        self.table.setColumnCount(columns)
-        self.table.setHorizontalHeader(headers)
+        self.table.setColumnCount(len(headers))
+        self.table.setHorizontalHeaderLabels(headers)
 
         layout.addWidget(self.table)
         self.setLayout(layout)
 
 class StaffTab(QWidget):
-    def __init__(self, text):
+    def __init__(self, pool):
         super().__init__()
         layout = QVBoxLayout()
 
-        self.label = QLabel(f"<h1>{text}</h1>")
-        layout.addWidget(self.label)
+        # Table
+        self.table = Tables(extractTable(pool, 'tbl_staff')[1])
+        layout.addWidget(self.table)
 
         self.setLayout(layout)
 
