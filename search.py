b68bc3be0c82dd9f78fd9124c968de1be2bcae4d
JamesBullen <james.roberts.bullen@gmail.com>
2025-04-07 22:09:13 +0100

    Started search widget

---
+++ search.py
@@ -0,0 +1,6 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTableWidgetItem, QGridLayout, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton, QComboBox, QMessageBox, QErrorMessage, QFrame
+
+# search widget class go here
+class Search(QWidget):
+    def __init__(self):
+        super().__init__()
