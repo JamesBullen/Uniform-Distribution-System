7f2c23812cc27551e8d02d4ca612fb3b764d0faf
JamesBullen <james.roberts.bullen@gmail.com>
2025-04-08 07:51:52 +0100

    Added table functionality and start of order inputs

--- orders.py
+++ orders.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTableWidgetItem, QGridLayout, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton, QComboBox, QMessageBox, QErrorMessage, QFrame
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTableWidgetItem, QFormLayout, QGridLayout, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton, QComboBox, QMessageBox, QErrorMessage, QFrame, QCheckBox
+from PyQt6.QtGui import QIntValidator
 from database import getValidtionTable, callProcedure
 from table import Table
 
@@ -6,16 +7,91 @@ class OrdersTab(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
+        self.labelDict = {1: None, 2: None, 3: None, 4: None}
+        self.inputDict = {1: None, 2: None, 3: None, 4: None}
+
+        # Table
+        self.table = Table("call OrderInfo(%s)", 1)
 
         # Buttons
         tableButtons = QHBoxLayout()
         # New order
         #* select role, brings up all uniforms of that role, then select size and quantity
+        self.newOrderButton = QPushButton('Place an order', self)
+        self.newOrderButton.clicked.connect(self.showSelectionForm)
+        tableButtons.addWidget(self.newOrderButton)
         # Available reissues
-        # 
+        # Refresh
+        refreshBut = QPushButton('Refresh')
+        refreshBut.clicked.connect(self.table.updateTable)
+        tableButtons.addWidget(refreshBut)
+        layout.addLayout(tableButtons)
 
-        # Table
-        self.table = Table("call OrderInfo(%s)", 1)
+        # Staff selection form
+        self.selectionLayout = QGridLayout()
+        self.selectionFrame = QFrame()
+        labelCol = QVBoxLayout()
+        inputCol = QVBoxLayout()
+        staffBut = QHBoxLayout()
+        self.staffInput = QComboBox() # Needs to be declared here due to role affecting it
+        # Role search
+        roleLabel = QLabel('Role:')
+        labelCol.addWidget(roleLabel)
+        self.roleInput = QComboBox()
+        roleResults = getValidtionTable('tbl_roles')
+        self.roleInput.addItems([i[1] for i in roleResults])
+        self.roleInput.setCurrentIndex(-1)
+        self.roleInput.currentIndexChanged.connect(lambda: self.loadStaff(self.roleInput.currentIndex()+1))
+        inputCol.addWidget(self.roleInput)
+        # Staff search
+        stafflabel = QLabel('Name:')
+        labelCol.addWidget(stafflabel)
+        inputCol.addWidget(self.staffInput)
+        # Buttons
+        nextBut = QPushButton('Next')
+        nextBut.clicked.connect(lambda: print('clicked'))
+        staffBut.addWidget(nextBut)
+        cancBut = QPushButton('Cancel')
+        cancBut.clicked.connect(lambda: self.selectionFrame.hide())
+        staffBut.addWidget(cancBut)
+        # Layout
+        self.selectionLayout.addLayout(labelCol, 0, 0)
+        self.selectionLayout.addLayout(inputCol, 0, 1)
+        self.selectionLayout.addLayout(staffBut, 1, 0, 1, 2)
+        self.selectionFrame.setLayout(self.selectionLayout)
+
+        # Uniform selection
+        self.uniformLayout = QFormLayout()
+        self.uniformFrame = QFrame()
+        uniformBut = QHBoxLayout()
+        # test
+        testy = QLineEdit()
+        validator = QIntValidator(1, 99, self)
+        testy.setValidator(validator)
+        # Layout
+        self.uniformFrame.setLayout(self.uniformLayout)
+
         layout.addWidget(self.table)
-
         self.setLayout(layout)
+
+    def showSelectionForm(self):
+        self.roleInput.setCurrentIndex(-1)
+        self.staffInput.clear()
+        self.selectionFrame.show()
+        return
+
+    def loadStaff(self, role):
+        staffResult = callProcedure('call FindStaff(%s)', role)[0]
+
+        self.staffInput.clear()
+        self.staffInput.addItems([f"{i[1]}, ID: {i[0]}" for i in staffResult])
+
+        if len(staffResult) == 1:
+            self.generateSelection()
+            return
+        
+        self.staffInput.setCurrentIndex(-1)
+        return
+    
+    def generateSelection(self):
+        ...
