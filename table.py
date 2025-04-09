bb6e693d878e587618ac33e97ca325930b708f61
JamesBullen <james.roberts.bullen@gmail.com>
2025-04-09 23:47:17 +0100

    Tables can now return selected rows

--- table.py
+++ table.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QCheckBox
+from PyQt6.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout
+from PyQt6.QtCore import Qt
 from database import callProcedure
 
 class Table(QWidget):
@@ -11,7 +12,11 @@ class Table(QWidget):
         self.data = self.getData(args)
         self.table = QTableWidget()
         self.table.setColumnCount(len(self.data[1])-exclude if checks == False else len(self.data[1])-exclude+1)
-        self.table.setHorizontalHeaderLabels(self.data[1][exclude:])
+
+        if checks:
+            checkHeaders = list(self.data[1][exclude:])
+            checkHeaders.append('Select')
+        self.table.setHorizontalHeaderLabels(self.data[1][exclude:] if checks == False else checkHeaders)
         self.setTable(self.data[0][exclude:])
 
         layout.addWidget(self.table)
@@ -31,10 +36,30 @@ class Table(QWidget):
                 self.table.setItem(rowPos, i, QTableWidgetItem(str(r[i])))
             
             if self.checks:
-                self.table.setItem(rowPos, len(r)+1, QCheckBox())
+                checkbox = QTableWidgetItem()
+                checkbox.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
+                checkbox.setCheckState(Qt.CheckState.Unchecked)
+                self.table.setItem(rowPos, len(r), checkbox)
     
     def updateTable(self):
         rows = self.getData(self.table.rowCount() +1)[0]
         newRows = [r for r in rows if r not in self.data[0]]
         
         self.setTable(newRows)
+
+    def getSelectedRows(self):
+        if not self.checks:
+            return
+        
+        results = []
+        for row in range(self.table.rowCount()):
+            if self.table.item(row, self.table.columnCount()-1).checkState() == Qt.CheckState.Checked:
+                items = []
+                for col in range(self.table.columnCount() -1):
+                    item = self.table.item(row, col).text()
+                    items.append(item)
+                
+                results.append(items)
+
+        print(results)
+        return results
