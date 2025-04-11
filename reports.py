0eb7257e3af720e554842ac007ecdf8056869398
JamesBullen <james.roberts.bullen@gmail.com>
2025-04-11 03:15:07 +0100

    Reports can now display tables with css support for styling later

--- reports.py
+++ reports.py
@@ -1,28 +1,32 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QFormLayout, QPushButton, QHBoxLayout, QTableWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QFormLayout, QPushButton, QHBoxLayout, QTableWidget, QLabel
 from PyQt6.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
-from PyQt6.QtGui import QTextDocument, QTextCursor
+from PyQt6.QtGui import QTextDocument, QTextCursor, QTextTableFormat, QTextFrameFormat
 from database import getValidtionTable, callProcedure
 from table import Table
+from PyQt6 import QtGui, QtCore
 
 class ReportsTab(QWidget):
     def __init__(self):
         super().__init__()
         layout = QFormLayout()
 
-        test = ReportRow()
-        layout.addRow('test', test)
+        test = ReportRow('Staff')
+        layout.addRow('Report:', test)
+
         self.setLayout(layout)
 
 class ReportRow(QHBoxLayout):
-    def __init__(self):
+    def __init__(self, label):
         super().__init__()
 
+        reportLabel = QLabel(label)
         # Buttons
         viewBut = QPushButton('View')
         viewBut.clicked.connect(self.viewReport)
         printBut = QPushButton('Print')
         printBut.clicked.connect(self.printReport)
 
+        self.addWidget(reportLabel)
         self.addWidget(viewBut)
         self.addWidget(printBut)
 
@@ -35,17 +39,38 @@ class ReportRow(QHBoxLayout):
         dialog = QPrintDialog()
         
         if dialog.exec() == QPrintDialog.accepted:
-            self.test.print(self.createDocument)
+            self.createDocument(dialog.printer())
     
     def createDocument(self, printer):
+        # Document setup
         doc = QTextDocument()
         cursor = QTextCursor(doc)
         baseTable = Table('call StaffInfo(%s)', 1)
-        docTable = cursor.insertTable(baseTable.rowCount(), baseTable.columnCount())
 
-        for row in range(docTable.rows()):
-            for col in range(docTable.columns()):
-                cursor.insertText(baseTable.item(row, col).text())
-                cursor.movePosition(QTextCursor.MoveOperation.NextCell)
+        # HTML
+        html = ['<table><thead><tr>']
+        # Headers
+        for header in range(baseTable.columnCount()):
+            html.append(f'<th>{baseTable.horizontalHeaderItem(header).text()}</th>')
+        # Rows
+        html.append('</tr></thead><tbody>')
+        for row in range(baseTable.rowCount()):
+            html.append('<tr>')
+            for col in range(baseTable.columnCount()):
+                print(row, col)
+                html.append(f'<td>{baseTable.item(row, col).text()}</td>')
+            html.append('</tr>')
+        html.append('</tbody></table>')
+
+        docTable = cursor.insertHtml(''.join(html))
 
         doc.print(printer)
+
+# for header in range(baseTable.columnCount()):
+#             cursor.insertText(baseTable.horizontalHeaderItem(header).text())
+#             cursor.movePosition(QTextCursor.MoveOperation.NextCell)
+
+#         for row in range(docTable.rows() -1):
+#             for col in range(docTable.columns()):
+#                 cursor.insertText(baseTable.item(row, col).text())
+#                 cursor.movePosition(QTextCursor.MoveOperation.NextCell)
